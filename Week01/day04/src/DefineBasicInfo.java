fe65301def46f768c1b527d87c7416fb9763f535
Anna882 <toth0anna@gmail.com>
2018-02-15 16:21:44 +0100

    java(define basic info)feat

---
+++ Week01/day04/src/DefineBasicInfo.java
@@ -0,0 +1,13 @@
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+        // Define several things as a variable then print their values
+        // Your name as a string
+        String myNAme = "Anna";
+        // Your age as an integer
+        int age = 29;
+        // Your height in meters as a double
+        double height = 1.71;
+        // Whether you are married or not as a boolean
+        boolean married = false;
+    }
+}
