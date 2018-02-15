12a9c940ac75c26092f49ece7f67d17bcac2aba3
Anna882 <toth0anna@gmail.com>
2018-02-15 16:06:57 +0100

    javapractice(codinghours)feat

--- Week01/day04/src/CodingHours.java
+++ Week01/day04/src/CodingHours.java
@@ -5,8 +5,13 @@ public class CodingHours {
         //
         // Print how many hours is spent with coding in a semester by an attendee,
         // if the attendee only codes on workdays.
-        
+        int coding = (17 * 5 * 6);
+        System.out.println("An attendee spend " + coding + " hours with coding in a semester.");
         // Print the percentage of the coding hours in the semester if the average
         // work hours weekly is 52
+        float workingHours;
+        workingHours = 52 * 17;
+        float valid = coding / workingHours;
+        System.out.println("Coding hours is " + valid * 100 + " percentage in the semester");
     }
 }
