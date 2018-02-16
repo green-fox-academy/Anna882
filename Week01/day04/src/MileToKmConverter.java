879e43ac4799418815bbbaa6298c91de30ad0019
Anna882 <toth0anna@gmail.com>
2018-02-16 10:23:11 +0100

    javapractice(mile to km converter)feat

--- Week01/day04/src/MileToKmConverter.java
+++ Week01/day04/src/MileToKmConverter.java
@@ -5,5 +5,14 @@ public class MileToKmConverter {
         // Write a program that asks for an integer that is a distance in kilometers,
         // then it converts that value to miles and prints it
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please type a distance in kilometers as an integer!");
+        int kilometers = scanner.nextInt();
+        double aMile = 0.621371192;
+        double miles;
+
+        miles = kilometers * aMile;
+        System.out.println("The distance is " + miles + " miles.");
+
     }
 }
