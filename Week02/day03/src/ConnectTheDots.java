8591fd4a1d602df267e5a75a319627352310a492
Anna882 <toth0anna@gmail.com>
2018-02-21 17:16:26 +0100

    feat(ConnectTheDots)draw

---
+++ Week02/day03/src/ConnectTheDots.java
@@ -0,0 +1,69 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class ConnectTheDots {
+    public static void mainDraw(Graphics graphics) {
+        // Create a function that takes 1 parameter:
+        // An array of {x, y} points
+        // and connects them with green lines.
+        // Connect these to get a box: {{10, 10}, {290,  10}, {290, 290}, {10, 290}}
+        // Connect these: {{50, 100}, {70, 70}, {80, 90}, {90, 90}, {100, 70},
+        // {120, 100}, {85, 130}, {50, 100}}
+        int[][] points1 = {{10, 10}, {290,  10}, {290, 290}, {10, 290}};
+        int[][] points2 = {{50, 100}, {70, 70}, {80, 90}, {90, 90}, {100, 70}, {120, 100}, {85, 130}, {50, 100}};
+
+        connectWithGreenLine(points1, graphics);
+        connectWithGreenLine(points2, graphics);
+    }
+
+
+    public static void connectWithGreenLine(int[][] points, Graphics graphics) {
+        graphics.setColor(Color.GREEN);
+        int x1, x2, y1, y2;
+        graphics.drawLine(points[points.length-1][0], (points[points.length-1][1]), points[0][0], points[0][1]);
+        for (int i = 0; i < points.length - 1; i++){
+            x1 = points[i][0];
+            y1 = points[i][1];
+            x2 = points[i+1][0];
+            y2 = points[i+1][1];
+            graphics.drawLine(x1, y1, x2, y2);
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 343;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jFrame.add(new ImagePanel());
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
+
+
+// create a 300x300 canvas.
