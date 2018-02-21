fc1ce8a489755f58ef912dfc221e2f48513e481b
Anna882 <toth0anna@gmail.com>
2018-02-21 12:14:57 +0100

    feat(PositionSquare) draw

--- Week02/day03/src/PositionSquare.java
+++ Week02/day03/src/PositionSquare.java
@@ -4,32 +4,28 @@ import java.awt.*;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-public class FourRectangles {
+public class PositionSquare {
 
     public static void mainDraw(Graphics graphics){
-        // draw four different size and color rectangles.
+        // create a square drawing function that takes 2 parameters:
+        // the x and y coordinates of the square's top left corner
+        // and draws a 50x50 square from that point.
+        // draw 3 squares with that function.
         // avoid code duplication.
-        String[] colors = {"red", "blue", "green", "yellow"};
-        for (int i = 0; i < 4; i++) {
-            int x = (int) (Math.random() * (WIDTH - 2) + 1);
-            int y = (int) (Math.random() * (HEIGHT -2) + 1);
-            int width = (int) (Math.random() * (WIDTH - x));
-            int height = (int) (Math.random() * (WIDTH - y));
-            String color = colors[i];
-            drawRectangle(x, y, width, height, color,  graphics);
+        for (int i = 0; i < 3; i++) {
+            int x = (int) (Math.random() * (WIDTH - 50) + 1);
+            int y = (int) (Math.random() * (HEIGHT -50) + 1);
+        drawSquare(x, y, graphics);
         }
-
-
     }
-
-    public static void drawRectangle(int x, int y, int width, int height, String inputColor, Graphics graphics) {
-        graphics.setColor(Color.getColor(inputColor));
-        graphics.drawRect(x, y, width, height);
+    public static void drawSquare(int x, int y, Graphics graphics) {
+        graphics.drawRect(x, y, 50, 50);
     }
 
     //    Don't touch the code below
     static int WIDTH = 320;
     static int HEIGHT = 343;
+
     public static void main(String[] args) {
         JFrame jFrame = new JFrame("Drawing");
         jFrame.setSize(new Dimension(WIDTH, HEIGHT));
