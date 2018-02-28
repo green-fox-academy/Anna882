a4ddef6a63282157d0a061ac60af25f5f1208dd5
Anna882 <toth0anna@gmail.com>
2018-02-28 11:51:42 +0100

    feat(BlogPost)run

---
+++ Week03/day03/src/BlogPost/BlogPost.java
@@ -0,0 +1,15 @@
+package BlogPost;
+
+public class BlogPost {
+  private String authorName;
+  private String title;
+  private String text;
+  private String publicatoionDate;
+
+  public BlogPost(String authorName, String title, String text, String publicatoionDate) {
+    this.authorName = authorName;
+    this.title = title;
+    this.text = text;
+    this.publicatoionDate = publicatoionDate;
+  }
+}
