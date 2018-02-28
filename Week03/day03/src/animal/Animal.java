a1201085912caac648ce21a56bf08f7003870050
Anna882 <toth0anna@gmail.com>
2018-02-28 14:58:35 +0100

    feat(Animal)run

---
+++ Week03/day03/src/animal/Animal.java
@@ -0,0 +1,21 @@
+package animal;
+
+public class Animal {
+  private int hunger;
+  private int thirst;
+
+  public Animal(){
+    hunger = 50;
+    thirst = 50;
+  }
+  public void eat(){
+    hunger--;
+  }
+  public void drink(){
+    thirst--;
+  }
+  public void play(){
+    hunger++;
+    thirst++;
+  }
+}
