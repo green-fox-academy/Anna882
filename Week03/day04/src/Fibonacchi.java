3c2f804df081ba28f6c224322d35559b750fb386
Anna882 <toth0anna@gmail.com>
2018-03-01 14:49:55 +0100

    feat(Fibonacchi)run

---
+++ Week03/day04/src/Fibonacchi.java
@@ -0,0 +1,14 @@
+public class Fibonacchi {
+  public static void main(String[] args) {
+    int nthNumber = 7;
+    System.out.println(fibonacchiFac(nthNumber));
+  }
+  public static int fibonacchiFac(int nthNumber){
+    if (nthNumber <= 2){
+      return 1;
+    } else {
+      nthNumber = fibonacchiFac(nthNumber - 1) + fibonacchiFac(nthNumber - 2);
+      return nthNumber;
+    }
+  }
+}
