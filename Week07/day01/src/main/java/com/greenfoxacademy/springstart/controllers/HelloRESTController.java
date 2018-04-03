cc1b1f9e791d3a66de4361e2735ae6582def5a9d
Anna882 <toth0anna@gmail.com>
2018-04-03 12:54:37 +0200

    feat(HelloRestController)greeting user method return type changed

--- Week07/day01/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ Week07/day01/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -8,8 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloRESTController {
 
   @RequestMapping(value = "/greeting")
-  public String greetingUser(@RequestParam("name") String userName) {
-    Greeting greeting = new Greeting(1, "Hello, " + userName);
-    return greeting.toString();
+  public Greeting greetingUser(@RequestParam("name") String userName) {
+    return new Greeting(1, "Hello, " + userName);
   }
 }
